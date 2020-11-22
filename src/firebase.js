62570e6a53ae4c56491d4d97a43f92c9d243f4a4
KLEWIS <22304006+kombolewis@users.noreply.github.com>
2020-11-22 09:31:02 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3f8XUWWZs1xvvzl4o5uXRJW2VYFsLMY4",
-  authDomain: "astrx-59bd7.firebaseapp.com",
-  databaseURL: "https://astrx-59bd7.firebaseio.com",
-  projectId: "astrx-59bd7",
-  storageBucket: "astrx-59bd7.appspot.com",
-  messagingSenderId: "241976776699",
-  appId: "1:241976776699:web:3dfb1823746a2bf4dc1d5e",
-  measurementId: "G-L9LTTJ992D"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 // Initialize Firebase
 const fb = firebase.initializeApp(firebaseConfig);
